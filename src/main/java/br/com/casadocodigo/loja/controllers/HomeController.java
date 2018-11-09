19bb9ff733e0b02e4e490c70c892dad7a37a9e40
Robson Marinho <robson_luz15@hotmail.com>
2018-11-09 01:19:10 -0300

    Completando Controller

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/")	//Faz o mapeamento no "barra"
-	//método index
-	public void index() {
-		
+
+	@RequestMapping("/") // Faz o mapeamento no "barra"
+	// método index
+	public String index() {
 		System.out.println("Entrando na Home da CDC");
-		
+		return "home";
 	}
-	
+
 }
